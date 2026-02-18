9097cd025fe45159f44c50acabc90868ae90fd8a
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-02-18 21:01:59 +0300

    clearning up data fetching

--- src/AppCopy.js
+++ src/AppCopy.js
@@ -79,12 +79,14 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
             `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal },
           );
           if (!res.ok) {
             throw new Error("Something went wrong with fetching movies");
@@ -95,8 +97,11 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -109,6 +114,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query],
   );
